39b3d8ae2ec3d887a0a9d281cb4fd11a6a9fe30a
Zayd Hammoudeh <zayd.hammoudeh@gmail.com>
2018-12-05 20:10:50 -0800

    Fixed issues in install script the creates the symlinks

--- install_files.sh
+++ install_files.sh
@@ -2,20 +2,22 @@
 
 # Function for installing a file from then repository to the filesystem
 function install_file() {
+    SCRIPT=`realpath $0`
+    SCRIPTPATH=`dirname $SCRIPT`
     printf "Creating sylink for file: \"$1\"\n"
-    ln -sf $1 ~/$1
+    ln -sf ${SCRIPTPATH}/$1 ~/$1
 }
 
-install_file .gitconfig 
-install_file .pylintrc   
+install_file .gitconfig
+install_file .pylintrc
 install_file .vimrc
 install_file .zshrc
 
 # UltiSnips Plugin - LaTeX snippets
 install_file .vim/UltiSnips/tex.snippets
-# Python packages for Python Imports vim module 
+# Python packages for Python Imports vim module
 install_file .vim/python-imports.cfg
-# LaTeX specific vim settings 
+# LaTeX specific vim settings
 install_file .vim/ftplugin/tex.vim
 # Settings for latexmk Perl script
-install_file ./latexmkrc
+install_file .latexmkrc

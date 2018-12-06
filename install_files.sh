1842f3a4f7931968923417748e8470ddea65148d
Zayd Hammoudeh <zayd.hammoudeh@gmail.com>
2018-12-05 20:00:27 -0800

    Renamed the .vimrc and .zshrc. Added more vim files including to the install script.

--- install_files.sh
+++ install_files.sh
@@ -2,15 +2,20 @@
 
 # Function for installing a file from then repository to the filesystem
 function install_file() {
-    ARG_FILE=`basename $1`
-    ARG_PATH=`dirname $1`
-    DEST_PATH=.${ARG_FILE}
-    if [[ -z ${ARG_PATH} ]]; then
-        DEST_PATH=${ARG_PATH}/${DEST_PATH}
-    fi
-    printf "Creating sylink for file: \"${DEST_PATH}\"\n"
-    ln -sf $1 ~/${DEST_PATH}
+    printf "Creating sylink for file: \"$1\"\n"
+    ln -sf $1 ~/$1
 }
 
-install_file vimrc
-install_file zshrc
+install_file .gitconfig 
+install_file .pylintrc   
+install_file .vimrc
+install_file .zshrc
+
+# UltiSnips Plugin - LaTeX snippets
+install_file .vim/UltiSnips/tex.snippets
+# Python packages for Python Imports vim module 
+install_file .vim/python-imports.cfg
+# LaTeX specific vim settings 
+install_file .vim/ftplugin/tex.vim
+# Settings for latexmk Perl script
+install_file ./latexmkrc

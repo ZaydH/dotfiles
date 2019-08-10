1b1b3fb6f2eb6068056f4b582051e30309c041f6
Zayd Hammoudeh <zayd.hammoudeh@gmail.com>
2019-08-10 15:29:24 -0700

    setup_script: Added pyenv-virtualenv helper module that links pyenv and virtualenv

--- setup_script.sh
+++ setup_script.sh
@@ -77,6 +77,7 @@ function install_all_packages() {
                      gzip bzip2 p7zip zlib1g-dev
                      fzf
                      python python3 python-dev python3-dev python-devel ipython jupyter
+                     pyenv-virtualenv  # Combines pyenv with virtualenv. May be brew specific
                      coreutils moreutils zsh tmux libomp libomp-dev
                      openssl libssl-dev readline readline-devel libreadline7
                      libreadline7-dev libreadline6-dev sqlite3 libsqlite3-dev

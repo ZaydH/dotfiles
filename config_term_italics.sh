aaf8adf8e2199d90c349eb0df79c75eacc1863aa
Zayd Hammoudeh <zayd.hammoudeh@gmail.com>
2020-03-13 19:37:59 -0700

    config_term_italics.sh: Added test function to italics script.

--- config_term_italics.sh
+++ config_term_italics.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
-# Based on: https://medium.com/@dubistkomisch/how-to-actually-get-italics-and-true-colour-to-work-in-iterm-tmux-vim-9ebe55ebc2be
+function test_italics() {
+    printf "If italics is working, the following should be italics...\n"
+    echo -e "\e[3mI am italics.  If I am not italics, you have a problem\e[23m"
+}
 
-TERM_DIR=terminal_info
+function configure_term_info(){
+    # Based on: https://medium.com/@dubistkomisch/how-to-actually-get-italics-and-true-colour-to-work-in-iterm-tmux-vim-9ebe55ebc2be
+    TERM_DIR=terminal_info
 
-tic -x ${TERM_DIR}/xterm-256color-italic.terminfo
-tic -x ${TERM_DIR}/tmux-256color.terminfo
+    tic -x ${TERM_DIR}/xterm-256color-italic.terminfo
+    tic -x ${TERM_DIR}/tmux-256color.terminfo
+}

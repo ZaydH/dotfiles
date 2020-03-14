5377a1fa33f52793a5c2b119644015182db15b7b
Zayd Hammoudeh <zayd.hammoudeh@gmail.com>
2020-03-13 18:11:52 -0700

    config_term_italics.sh: Added comment to document where setup came from

--- config_term_italics.sh
+++ config_term_italics.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Based on: https://medium.com/@dubistkomisch/how-to-actually-get-italics-and-true-colour-to-work-in-iterm-tmux-vim-9ebe55ebc2be
+
 TERM_DIR=terminal_info
 
 tic -x ${TERM_DIR}/xterm-256color-italic.terminfo

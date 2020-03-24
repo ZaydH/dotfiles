2766c945d33c54c0d0e4237c5af0d5fa428e5f22
Zayd Hammoudeh <zayd.hammoudeh@gmail.com>
2020-03-24 15:28:32 -0700

    install_zsh.sh: Added command to change shell to zsh by default.

--- install_zsh.sh
+++ install_zsh.sh
@@ -3,6 +3,9 @@
 TARGET_ANTIGEN=${HOME}/antigen.zsh
 
 function install_oh_my_zsh() {
+    chsh -s $(which zsh)
+    printf "Changed default terminal to zsh...May require a restart before setting applied.\n"
+
     printf "Copying ${TARGET_ANTIGEN}..."
     rm -rf ${TARGET_ANTIGEN} > /dev/null
     curl -L --silent --show-error git.io/antigen > ${TARGET_ANTIGEN}

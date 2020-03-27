cd6b9572b754f4f11e315a8e5d79dde9e3d320d8
Zayd Hammoudeh <zayd.hammoudeh@gmail.com>
2020-03-27 07:56:34 -0700

    link_files.sh: Removing ssh from linking.

--- link_files.sh
+++ link_files.sh
@@ -84,8 +84,3 @@ install_file ${TMUX_CONF_LOCAL}
 install_file .tmux
 # # Configuration for google cloud
 # install_file .config/gcloud/configurations/config_default
-# Add the file containing ssh/scp configuration
-if is_mac; then
-    install_file .ssh/config
-fi
-install_file .ssh/id_rsa.pub

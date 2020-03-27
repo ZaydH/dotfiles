a529526b09ebe60a4c5f1472d4ff3183c89d9b0f
Zayd Hammoudeh <zayd.hammoudeh@gmail.com>
2020-03-26 19:11:08 -0700

    setup_mac_only.sh: Adding iterm2 script

---
+++ setup_mac_only.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+source .functions
+
+function install_iterm2() {
+    brew cask install iterm2
+}
+
+function install_mac_fonts() {
+    # Install and Configure Fira Code Font
+    brew tap homebrew/cask-fonts
+    brew cask install font-fira-code
+}
+
+function run_mac_only_setup(){
+    if ! is_mac; then
+        printf "Running Mac-only installer on a system that appears to not be a mac...Exiting" >&2
+        return 1
+    fi
+    install_iterm2
+
+    install_mac_fonts
+}
--- setups_mac_only.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# Install and Configure Fira Code Font
-brew tap homebrew/cask-fonts
-brew cask install font-fira-code

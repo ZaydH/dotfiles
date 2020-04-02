79badaf13915a5fd87ae04c78f199c893152ba94
Zayd Hammoudeh <zayd.hammoudeh@gmail.com>
2020-04-02 12:09:12 -0700

    setup_gnome_only.sh: Added code to set gnome terminal colorscheme

--- setup_gnome_only.sh
+++ setup_gnome_only.sh
@@ -7,3 +7,18 @@ function make_gnome_look_like_mac() {
     install_cli_package gtk2-engines-murrine
     install_cli_package gtk2-engines-pixbuf
 }
+
+function configure_gnome_terminal_colorscheme() {
+    printf "Configuring gnome colorscheme...\n"
+
+    source .functions
+    # Packages needed by Gogh
+    install_cli_package dconf-cli
+    install_cli_package uuid-runtime
+
+    # printf "Reset gnome legacy terminal profiles...\n"
+    # dconf reset -f /org/gnome/terminal/legacy/profiles:/
+
+    printf "Installing Gogh colorscheme tool...\n"
+    bash -c  "$(wget -qO- https://git.io/vQgMr)"
+}

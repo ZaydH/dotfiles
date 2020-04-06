54466356564f85262359609f7d8fd96738fdcec9
Zayd Hammoudeh <zayd.hammoudeh@gmail.com>
2020-04-06 05:49:39 -0700

    setup_gnome_only.sh: More work on Pop! OS

--- setup_gnome_only.sh
+++ setup_gnome_only.sh
@@ -22,3 +22,35 @@ function configure_gnome_terminal_colorscheme() {
     printf "Installing Gogh colorscheme tool...\n"
     bash -c  "$(wget -qO- https://git.io/vQgMr)"
 }
+
+# Enables the gnome keyring to save the git credentials
+function configure_git_for_gnome_keyring() {
+    install_cli_package libgnome-keyring-dev
+    GIT_KEYRING_DIR="/usr/share/doc/git/contrib/credential/gnome-keyring"
+    sudo make --directory="${GIT_KEYRING_DIR}"
+
+    CREDENTIAL_FILE="${GIT_KEYRING_DIR}/git-credential-gnome-keyring"
+    git config --global credential.helper "${CREDENTIAL_FILE}"
+}
+
+function configure_libreoffice_packages() {
+    install_cli_package libreoffice-java-common
+}
+
+function configure_appmenu_packages() {
+    install_cli_package appmenu-gtk3-module
+}
+
+
+# Encapsulates all the individual operations into a single function
+function run_mac_only_setup(){
+    make_gnome_look_like_mac
+
+    configure_gnome_terminal_colorscheme
+
+    configure_git_for_gnome_keyring
+
+    configure_appmenu_packages
+
+    configure_libreoffice_packages
+}

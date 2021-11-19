e2dcde13dddbc08ba4cc78923a460c60dedec087
Zayd Hammoudeh <zayd.hammoudeh@gmail.com>
2021-11-19 13:17:01 -0800

    install_python.sh: Updated default torch version

--- install_python.sh
+++ install_python.sh
@@ -27,7 +27,7 @@ function install_python_linters() {
 
 
 function install_3090_torch() {
-    pip install torch==1.8.0+cu111 torchvision==0.9.0+cu111 torchaudio==0.8.0 \
+    pip install torch==1.8.1+cu111 torchvision==0.9.1+cu111 torchaudio==0.8.1 \
         -f https://download.pytorch.org/whl/torch_stable.html
 }
 
